fed445319416abaffc173927e35c0936d762b89a
AkarshikaCharu <akarshikacharu257@gmail.com>
2025-11-09 17:16:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import gzip
 
 # Load data
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+similarity = pickle.load(gzip.open('similarity_compressed.pkl.gz', 'rb'))
+
 
 # TMDB API key
 API_KEY = "ea98cca847a87d579367a4549194c089"
